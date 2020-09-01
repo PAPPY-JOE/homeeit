447b3f6375b99c90a997e6354f4642ad33cb17d9
PAPPY-JOE <fatoyejoseph@gmail.com>
2020-09-01 19:25:13 +0100

    Added onload

---
+++ scripts/onload.js
@@ -0,0 +1,6 @@
+let onload = function () {
+    $('.block1').css('animation', 'onload1 1s');
+    $('.block2').css('animation', 'onload2 1s');
+    $('.block3').css('animation', 'onload3 1s');
+    $('.onload').delay(900).slideUp(500);
+}
